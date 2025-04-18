f1793b6a894493896348eff368a91e346c9b3f7e
olegjbac <karakulin@astdesign.ru>
2025-04-18 16:07:18 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig({
-  base: '/vue3-todo-app/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command}) => ({
+    base: command === 'serve' ? '/' : '/vue3-todo-app/',
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/styles/index.scss" as *;`
-      }
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/assets/styles/index.scss" as *;`
+            }
+        }
     }
-  }
-})
+}))
