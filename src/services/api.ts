56a4c4140fa1e0e5c0e4408c92112bb8a218ab85
olegjbac <karakulin@astdesign.ru>
2025-04-18 15:47:00 +0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ export interface Todo {
 }
 
 export function fetchTodos(): Promise<Todo[]> {
-    return fetch('https://jsonplaceholder.typicode.com/todo')
+    return fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Не удалось загрузить задачи. Статус: ${response.status}`);
